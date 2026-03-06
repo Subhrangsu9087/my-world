8f9a2381b0de67da3fefb379b17c2b857cd09e04
Subhrangsu <subhrangsubera@webviotechnologies.com>
2026-03-06 15:44:58 +0530

    style libery modified

---
+++ projects/dist/design-system/index.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from '@angular/core';
+
+declare class DesignSystem {
+    static ɵfac: i0.ɵɵFactoryDeclaration<DesignSystem, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<DesignSystem, "lib-design-system", never, {}, {}, never, never, true, never>;
+}
+
+export { DesignSystem };
--- projects/portfolio/src/app/app.routes.ts
+++ projects/portfolio/src/app/app.routes.ts
@@ -4,5 +4,15 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./app').then((m) => m.App),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'projects',
+        loadComponent: () => import('./features/projects/projects').then((m) => m.Projects),
+      },
+    ],
   },
 ];
